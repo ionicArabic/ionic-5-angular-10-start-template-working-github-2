8242647382ea55dfe28e52ad2dc2d74218bb6a71
ionicArabic <ionicarabic@gmail.com>
2021-06-13 14:33:47 +0300

    Tab4 updated

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -3,8 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab4Page } from './tab4.page';
-
+import { Tab4Page } from './tab4.page'
 @NgModule({
   imports: [
     IonicModule,
